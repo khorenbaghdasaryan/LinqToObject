19f6d88e0c149adf19aa4d2485e40c997791a6a9
Khoren Baghdasaryan <khoren.a.baghdasaryan@gmail.com>
2020-04-20 23:07:23 +0400

    1 Commit

---
+++ LinqToObject/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqToObject
+{
+    class Q
+    {
+        public void Query()
+        {
+            string text = @"Historically, the world of data and the world of objects" +
+          @" have not been well integrated. Programmers work in C# or Visual Basic" +
+          @" and also in SQL or XQuery. On the one side are concepts such as classes," +
+          @" objects, fields, inheritance, and .NET Framework APIs. On the other side" +
+          @" are tables, columns, rows, nodes, and separate languages for dealing with" +
+          @" them. Data types often require translation between the two worlds; there are" +
+          @" different standard functions. Because the object world has no notion of query, a" +
+          @" query can only be represented as a string without compile-time type checking or" +
+          @" IntelliSense support in the IDE. Transferring data from SQL tables or XML trees to" +
+          @" objects in memory is often tedious and error-prone.";
+
+            string searchTerm = "data";
+
+            string[] source = text.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var matchQuery = from word in source
+                             where word.ToLowerInvariant() == searchTerm.ToLowerInvariant()
+                             select word;
+
+            int wordCount = matchQuery.Count();
+
+            Console.WriteLine($"{wordCount} occurrences(s) of the search term  {searchTerm}");
+
+            string[] sentences = text.Split(new char[] { '.', '?', '!' });
+
+            string[] wordsToMatch = { "Historically", "data", "integrated" };
+
+            var sentenceQuery = from sentence in sentences
+                                let w = sentence.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',' },
+                                                        StringSplitOptions.RemoveEmptyEntries)
+                                where w.Distinct().Intersect(wordsToMatch).Count() == wordsToMatch.Count()
+                                select sentence;
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Q().Query();
+        }
+    }
+}
